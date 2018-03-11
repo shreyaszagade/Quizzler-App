7b9e9611a449831667f02dcf178dd217b3d2c08e
Shreyas Zagade <shreyaszagade@gmail.com>
2018-03-10 19:08:59 -0500

    Created UI and linked to view Controller

--- Quizzler App/ViewController.swift
+++ Quizzler App/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBarView: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func answerPressed(_ sender: UIButton) {
+    }
+    
 }
 
